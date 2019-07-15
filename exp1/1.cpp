53f58098e67598e2f8d0f8fd005e9ef50f255bd6
Jocia Mendes <16co28@aitdgoa.edu.in>
2019-07-15 15:54:47 +0530

    first cpp program

---
+++ exp1/1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string a;
+    int i;
+    cout<<"enter the string:";
+    getline(cin,a);
+    if((a>=65 && a<=90))
+    {
+        cout<< "alphbet";
+    }
+    else if((a>=48 && a<=57))
+    {
+        cout<<"digit";
+    }
+     else
+     {
+        cout << " Special Character ";
+     }
+}
+
